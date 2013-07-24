cacd7ac8d9dcb86bda49ee529e2ed4f7d5a971a4
elimumford <elimumford@gmail.com>
2013-07-24 14:19:40 -0600

    Bug fixes; Added logging

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib');
+var lib = require('./lib')
+, pkg   = require('./package')
+;
+
+Object.defineProperties(module.exports, {
+	version: { value: pkg.version, enumerable: true }
+	Repo: { value: lib, enumerable: true }
+});
