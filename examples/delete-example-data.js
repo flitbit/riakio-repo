a02dc50a25f19d748e1fb8134c34e6a46e6415b9
elimumford <elimumford@gmail.com>
2013-07-26 15:00:30 -0600

    bug fixes; working examples

---
+++ examples/delete-example-data.js
@@ -0,0 +1,36 @@
+var util = require('util')
+, winston = require('winston')
+, data = require('../node_modules/riakio/examples/practice-data')
+, repo = require('../lib/repo')
+, riakio = require('riakio')
+;
+
+riakio({servers: {
+	dev: {
+		scheme: 'http',
+		host: 'riakdev.netsteps.local',
+		port: 80
+	}
+}});
+
+var log = new (winston.Logger)({
+	transports: [new (winston.transports.Console)({ level: 'info' })]
+})
+, r = new repo({ bucket: 'flikr-photos'
+	, server: {name: 'dev' }
+	, calculateKey: function(item) {
+			return ''.concat(item.owner, '_', item.id);
+		}
+	, log: log })
+;
+
+var d = data.map(function(el) { return { value: el }; })
+;
+
+r.deleteMany(d, function(err, res){
+	if(err) {
+		log.error(util.inspect(err, true, 99));
+	} else {
+		log.info(util.inspect(res, false, 99));
+	}
+});
---
+++ examples/read-example-data.js
@@ -0,0 +1,43 @@
+var util = require('util')
+, winston = require('winston')
+, repo = require('../lib/repo')
+, riakio = require('riakio')
+;
+
+riakio({servers: {
+	dev: {
+		scheme: 'http',
+		host: 'riakdev.netsteps.local',
+		port: 80
+	}
+}});
+
+var log = new (winston.Logger)({
+	transports: [new (winston.transports.Console)({ level: 'info' })]
+})
+, r = new repo({ bucket: 'flikr-photos'
+	, server: {name: 'dev' }
+	, calculateKey: function(item) {
+			return ''.concat(item.owner, '_', item.id);
+		}
+	, log: log })
+;
+
+//{ "id": "9032192043", "owner": "10734133@N02", "secret": "e17ea45db8", "server": "7312", "farm": 8, "title": "the blue basket", "ispublic": 1, "isfriend": 0, "isfamily": 0},
+r.get('10734133@N02_9032192043', function(err, res){
+	if(err) {
+		log.error(err.stack);
+	} else {
+		log.info(util.inspect(res, false, 99));
+	}
+});
+
+//{ "id": "8621325545", "owner": "51225230@N03", "secret": "dd4a27c3a1", "server": "8116", "farm": 9, "title": "st ives", "ispublic": 1, "isfriend": 0, "isfamily": 0},
+r.get('51225230@N03_8621325545', function(err, res){
+	if(err) {
+		log.error(err.stack);
+	} else {
+		log.info(util.inspect(res, false, 99));
+	}
+});
+
